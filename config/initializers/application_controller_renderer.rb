34ddb9b99c11a0df7c115987cae35e1f8f664782
vagrant <vagrant@localhost.localdomain>
2021-01-05 08:15:00 +0000

    Added index&new_pages

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = 'e1ee7309ffc7c2ed69af56ecf274fe48fc6c9bbbbd011e960a7c54ebc455a9edaab1da51eed2864d5b8958117206f04c5c2fe90dadd098501537b33791546d97'
